09f08a68ce51818c59c282ab848f42570927e31c
erhan21-cmd <77546795+gulerhan@users.noreply.github.com>
2021-09-27 20:42:46 +0300

    I prepared add product app for vuejs

---
+++ product add app/src/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="container">
+    <h1 class="text-center">Ürün Ekleme Uygulaması</h1>
+    <hr>
+    <app-new-product></app-new-product>
+    <br><br>
+    <app-progress></app-progress>
+    <br><br>
+    <app-products></app-products>
+
+  </div>
+</template>
+
+<script>
+  import newProduct from "./components/newProduct";
+  import progress from "./components/progress";
+  import products from "./components/products";
+
+  export default {
+    components : {
+      appNewProduct : newProduct,
+      appProgress : progress,
+      appProducts : products,
+    }
+  }
+</script>
+
+<style>
+  body {
+    background-color: aliceblue;
+    padding-top: 20px;
+    padding-bottom: 20px;
+  }
+
+  .card{
+    margin-right: 5px;
+    margin-bottom: 5px;
+  }
+
+  .card:last-child{
+    margin-right: 0px;
+  }
+
+  .col-md-2{
+    max-width: 15.666667%!important;
+  }
+
+  .product-container{
+    margin-left: 15px;
+  }
+
+</style>
+
